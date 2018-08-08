4f40d4b17563f3ae0ccf5622ff8999a7167c2c58
hramn1 <stmihailov@yandex.ru>
2018-08-08 16:44:34 +0300

    правки

--- js/forma-zakaza.js
+++ js/forma-zakaza.js
@@ -1,12 +1,17 @@
 (function () {
   let shaterChoose = document.querySelectorAll('.shatri-item');
   let moadalChooseShater = document.querySelector('.choose-shater');
+  let formaZakaza = document.querySelector('.zakaz-forma');
+  let modalOver = document.querySelector('.modal-overlay');
+  let goodDiv = document.querySelector('.choose-goods');
   for (let i = 0; i < shaterChoose.length; i++){
-    shaterChoose[i].addEventListener('click', function (evt) {
+    shaterChoose[i].addEventListener('click', displayShaterChoose);
+    function displayShaterChoose (evt) {
+      modalOver.style.display = 'block';
       if (shaterChoose[i].id === 'pag'){
         shaterPagoda()
       }
-    })
+    }
   }
   function shaterPagoda() {
     let pagoda9 = document.createElement('button');
@@ -21,13 +26,29 @@
     moadalChooseShater.appendChild(pagoda64);
     let buttonChooseShater = document.querySelectorAll('.choose-shater button');
     for (let i = 0; i < buttonChooseShater.length; i++){
-      //console.log(buttonChooseShater);
       buttonChooseShater[i].addEventListener('click', chooseGoods)
     }
   }
-  function chooseGoods(evt) {
+  function chooseGoods() {
+      moadalChooseShater.style.display = 'none';
+      goodDiv.style.display = 'block';
       let titleGood = document.querySelector('.title-goods');
+      let btnChoose = document.querySelector('.choose-goods__btn');
       titleGood.innerText = this.innerText;
-  }
+    while (moadalChooseShater.firstChild) {
+      moadalChooseShater.removeChild(moadalChooseShater.firstChild);
+    }
+      btnChoose.addEventListener('click', function (){
+        addGoodsInForm(titleGood);
+      });
 
+  }
+  function addGoodsInForm(titleGood) {
+    let template = document.querySelector('template').content.cloneNode(true);
+    template.querySelector('h2').textContent = titleGood.innerText;
+    modalOver.style.display = 'none';
+    goodDiv.style.display = 'none';
+    formaZakaza.style.display = 'block';
+    formaZakaza.appendChild(template);
+  }
 })();
