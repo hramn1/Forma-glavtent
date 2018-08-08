b4d6b9fca1bc8f15fe6bc39f1783d9c30b906e6a
Unknown <stmihailov@yandex.ru>
2018-08-08 23:36:39 +0300

    ghfdrb

--- js/forma-zakaza.js
+++ js/forma-zakaza.js
@@ -4,12 +4,16 @@
   let formaZakaza = document.querySelector('.zakaz-forma');
   let modalOver = document.querySelector('.modal-overlay');
   let goodDiv = document.querySelector('.choose-goods');
+  let btnChoose = document.querySelector('.choose-goods__btn');
   for (let i = 0; i < shaterChoose.length; i++){
     shaterChoose[i].addEventListener('click', displayShaterChoose);
     function displayShaterChoose (evt) {
       modalOver.style.display = 'block';
       if (shaterChoose[i].id === 'pag'){
         shaterPagoda()
+      }
+      else if (shaterChoose[i].id === 'sfera') {
+        shaterSfera()
       }
     }
   }
@@ -29,19 +33,36 @@
       buttonChooseShater[i].addEventListener('click', chooseGoods)
     }
   }
+  function shaterSfera() {
+    let sfera6d = document.createElement('button');
+    let sfera8d = document.createElement('button');
+    let sfera10d = document.createElement('button');
+    let sfera12d = document.createElement('button');
+    sfera6d.innerText ='Шатёр Сфера диаметром 6 метров';
+    sfera8d.innerText ='Шатёр Сфера диаметром 8 метров';
+    sfera10d.innerText ='Шатёр Сфера диаметром 10 метров';
+    sfera12d.innerText ='Шатёр Сфера диаметром 12 метров';
+    moadalChooseShater.style.display = 'block';
+    moadalChooseShater.appendChild(sfera6d);
+    moadalChooseShater.appendChild(sfera8d);
+    moadalChooseShater.appendChild(sfera10d);
+    moadalChooseShater.appendChild(sfera12d);
+    let buttonChooseShater = document.querySelectorAll('.choose-shater button');
+    for (let i = 0; i < buttonChooseShater.length; i++){
+      buttonChooseShater[i].addEventListener('click', chooseGoods)
+    }
+  }
   function chooseGoods() {
-      moadalChooseShater.style.display = 'none';
-      goodDiv.style.display = 'block';
-      let titleGood = document.querySelector('.title-goods');
-      let btnChoose = document.querySelector('.choose-goods__btn');
-      titleGood.innerText = this.innerText;
+    moadalChooseShater.style.display = 'none';
+    goodDiv.style.display = 'block';
+    let titleGood = document.querySelector('.title-goods');
+    titleGood.innerText = this.innerText;
     while (moadalChooseShater.firstChild) {
       moadalChooseShater.removeChild(moadalChooseShater.firstChild);
     }
-      btnChoose.addEventListener('click', function (){
-        addGoodsInForm(titleGood);
-      });
-
+    btnChoose.onclick = function () {
+      addGoodsInForm(titleGood);
+    }
   }
   function addGoodsInForm(titleGood) {
     let template = document.querySelector('template').content.cloneNode(true);
