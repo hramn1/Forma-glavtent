9ef63c1cb64559386e18fbd1efb40fb1a05900d6
hramn1 <stmihailov@yandex.ru>
2018-08-17 17:25:19 +0300

    правки

--- js/forma-zakaza.js
+++ js/forma-zakaza.js
@@ -172,7 +172,7 @@
     let zvezda10d = document.createElement('button');
     let zvezda12d = document.createElement('button');
     zvezda10d.innerText ='Шатёр Звезда D10';
-    zvezda12d.innerText ='Шаётр Звезда D12';
+    zvezda12d.innerText ='Шатёр Звезда D12';
     moadalChooseShater.style.display = 'block';
     moadalChooseShater.appendChild(zvezda10d);
     moadalChooseShater.appendChild(zvezda12d);
@@ -236,7 +236,95 @@
     }
     switch(titleGood.innerText){
       case 'Шатёр Пагода 3 на 3':
-        imgSrc = 'img/pagoda.jpg'
+        imgSrc = 'img/pagoda.jpg';
+        break;
+      case 'Шатёр Пагода 5 на 5':
+        imgSrc = 'img/pagoda5.jpg';
+        break;
+      case 'Шатёр Пагода 64 кв.м':
+        imgSrc = 'img/pagoda64.jpg';
+        break;
+      case 'Шатёр Сфера диаметром 6 метров':
+        imgSrc = 'img/sfera6.jpg';
+        break;
+      case 'Шатёр Сфера диаметром 8 метров':
+        imgSrc = 'img/sfera8.jpg';
+        break;
+      case 'Шатёр Сфера диаметром 10 метров':
+        imgSrc = 'img/sfera10.jpg';
+        break;
+      case 'Шатёр Сфера диаметром 12 метров':
+        imgSrc = 'img/sfera12.jpg';
+        break;
+      case 'Мобильный шатёр 4*4':
+        imgSrc = 'img/mobilnie4.jpg';
+        break;
+      case 'мобильный шатёр 4*8':
+        imgSrc = 'img/mobilniy8.jpg';
+        break;
+      case 'Арочный Шатёр 5x5':
+        imgSrc = 'img/arc5.jpg';
+        break;
+      case 'Арочный Шатёр 6x6':
+        imgSrc = 'img/arc6.jpg';
+        break;
+      case 'Арочный Шатёр 8x8':
+        imgSrc = 'img/arc8.jpg';
+        break;
+      case 'Арочный Шатёр 8x8 Дюна':
+        imgSrc = 'img/duna8.jpg';
+        break;
+      case 'Арочный Шатёр 10x10':
+        imgSrc = 'img/arc10.jpg';
+        break;
+      case 'Арочный Шатёр 10x10 Дюна':
+        imgSrc = 'img/duna10.jpg';
+        break;
+      case 'Арочный Шатёр гексагональ 260 кв.м':
+        imgSrc = 'img/geksogonal260.jpg';
+        break;
+      case 'Арочный модуль 50 кв.м ':
+        imgSrc = 'img/modul50.jpg';
+        break;
+      case 'Шатёр Звезда D10':
+        imgSrc = 'img/zvezda10.jpg';
+        break;
+      case 'Шатёр Звезда D12':
+        imgSrc = 'img/zvezda12.jpg';
+        break;
+      case 'Шатёр Паук D11':
+        imgSrc = 'img/pauk11.jpg';
+        break;
+      case 'Шатёр Паук D13':
+        imgSrc = 'img/pauk13.jpg';
+        break;
+      case 'Шатёр Паук D17':
+        imgSrc = 'img/pauk17.jpg';
+        break;
+      case 'Шатёр Паук D19':
+        imgSrc = 'img/pauk19.jpg';
+        break;
+      case 'Шатёр Паук D21':
+        imgSrc = 'img/pauk21.jpg';
+        break;
+      case 'Шатёр Паук D26':
+        imgSrc = 'img/pauk26.jpg';
+        break;
+      case 'Тент шириной 6м':
+        imgSrc = 'img/tent6.jpg';
+        break;
+      case 'Тент шириной 8м':
+        imgSrc = 'img/tent8.jpg';
+        break;
+      case 'Тент шириной 10м':
+        imgSrc = 'img/tent10.jpg';
+        break;
+      case 'Тент шириной 15м':
+        imgSrc = 'img/tent15.jpg';
+        break;
+      case 'Тент шириной 20м':
+        imgSrc = 'img/tent20.jpg';
+        break;
     }
     template.querySelector('img').src = imgSrc;
     template.querySelector('h2').textContent = titleGood.innerText;
@@ -245,8 +333,14 @@
     template.querySelector('.goods-zakaz__kolvo').textContent = rangeKolvo.value;
     modalOver.style.display = 'none';
     goodDiv.style.display = 'none';
-    formaZakaza.style.display = 'block';
+    formaZakaza.style.display = 'flex';
     formaZakaza.appendChild(template);
     window.removeEventListener('keydown', onSuccessEscPress);
+    let btnRemoveItem = document.querySelectorAll('.goods-zakaz__remove');
+    for (let i = 0; i < btnRemoveItem.length; i++) {
+      btnRemoveItem[i].onclick = function removeItem() {
+        this.parentNode.remove()
+      }
+    }
   }
 })();
